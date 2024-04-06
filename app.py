4cdf06b660a2e709a4e4db8fcb6b1c5b1c335533
Josh <63469253+joshuatrobertson@users.noreply.github.com>
2024-04-06 23:26:27 +0100

    testing

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return "Hello!"
+
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run(port=5000)
