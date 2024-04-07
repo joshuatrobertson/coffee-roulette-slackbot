2abe31de48e0db176e2753c57f3e91ba36ec6807
jrobertson21 <josh.robertson@ibm.com>
2024-04-07 15:15:02 +0100

    pytorch added

--- ai_functions.py
+++ ai_functions.py
@@ -1,13 +1,16 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import pipeline
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Load the pre-trained GPT-2 model and tokenizer
-model_name = "gpt2"
-model = GPT2LMHeadModel.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+from transformers import pipeline
+
+instruct_pipeline = pipeline("text-generation", model="EleutherAI/mistral-7B-instruct-v0.2")
+
+# Generate text
+prompt = "Once upon a time"
+generated_text = instruct_pipeline(prompt, max_length=150, do_sample=True)
 
 
 def generate_weekly_message(date):
-    return "THIS IS A TEST" + date.strftime("%B %d, %Y")
+    return generated_text + date.strftime("%B %d, %Y")
