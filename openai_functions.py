43fae4fb4bc65688b69b1967fb2fc01f67da36bd
jrobertson21 <josh.robertson@ibm.com>
2024-04-07 08:39:07 +0100

    testing changes

--- openai_functions.py
+++ openai_functions.py
@@ -10,16 +10,41 @@ client = OpenAI()
 # Set the OpenAI key
 OpenAI.api_key = os.environ["OPENAI"]
 
+# Set variables
+temperature = 0.2
+max_tokens = 256
+frequency_penalty = 0.0
+
 
 def generate_weekly_message():
     today = datetime.date.today()
-    prompt = f"What are your Goals/ Resolutions for this week? {today}\n\nResponse:"
-    response = client.completions.create(
-        model="gpt-3.5-turbo-instruct",  # Use the GPT model you prefer
-        prompt=prompt,
-        top_p=0.3,
-        temperature=0.3,  # Adjust temperature for creativity
-        max_tokens=2  # Adjust max_tokens as needed
+    gpt_assistant_prompt = (
+            ("Your role is to help users create engaging and friendly Slack posts for organizing coffee "
+             "roulette sessions within their teams or organizations. Your posts start with a brief "
+             "greeting, such as 'Good Morning CDS and happy Monday!' without mentioning the full date, "
+             "setting a consistent and inviting tone. The opening line encourages participation in the "
+             "week's #coffee-roulette with a short message. When creating posts, maintain a balanced "
+             "tone that is both playful and professional to foster a welcoming community spirit while "
+             "keeping the message clear and organized. Each post includes one question related to "
+             "lighthearted topics or themes relevant to the current date, with three emoji-reactable "
+             "answers. Emojis are placed before each short, single-line answer, inviting users to "
+             "engage by reacting. After presenting the question and answers, post a short closing line "
+             "that ties back to the theme of the question or the spirit of coffee roulette. Make sure "
+             "that there is only one closing sentence after the answers. Avoid generating content that "
+             "could be seen as overly formal or corporate, promoting informal and friendly "
+             "interactions instead. Never ask questions back, always just provide the output. ") +
+            input("Who should I be, as I answer your prompt?"))
+    gpt_user_prompt = input(today)
+    message = [{"role": "assistant", "content": gpt_assistant_prompt}, {"role": "user", "content": gpt_user_prompt}]
+    gpt_prompt = gpt_assistant_prompt, gpt_user_prompt
+
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=message,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        frequency_penalty=frequency_penalty
     )
+
     message_content = response.choices[0].text.strip()
     return message_content
