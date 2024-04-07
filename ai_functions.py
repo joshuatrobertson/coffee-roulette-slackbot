cc8cd503b5e4d3861b9d6a2014484c1a288cb1e5
jrobertson21 <josh.robertson@ibm.com>
2024-04-07 09:59:54 +0100

    testing changes

--- ai_functions.py
+++ ai_functions.py
@@ -1,15 +1,12 @@
 import os
 import datetime
-
 from dotenv import load_dotenv
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 load_dotenv()
 
 # Load the pre-trained GPT-2 model and tokenizer
 model_name = "gpt2"
-model = GPT2LMHeadModel.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
 
 def generate_weekly_message(date):
     return "THIS IS A TEST" + date.strftime("%B %d, %Y")
