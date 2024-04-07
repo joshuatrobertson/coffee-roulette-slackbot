31ac39a1c601f60b4a97318c038024b1561d9ebe
jrobertson21 <josh.robertson@ibm.com>
2024-04-07 14:17:45 +0100

    pytorch added

--- ai_functions.py
+++ ai_functions.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 # Load the pre-trained GPT-2 model and tokenizer
 model_name = "gpt2"
-#model = GPT2LMHeadModel.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
 
 def generate_weekly_message(date):
