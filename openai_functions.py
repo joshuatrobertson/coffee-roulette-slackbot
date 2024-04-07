29c8b9e000574be72984c9340f392d0351eafbc7
jrobertson21 <josh.robertson@ibm.com>
2024-04-07 09:41:42 +0100

    testing changes

--- openai_functions.py
+++ openai_functions.py
@@ -1,21 +1,16 @@
+import os
+
 import openai
 import datetime
 
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
 # Set your OpenAI API key
-openai.api_key = 'your-api-key'
+openai.api_key = os.environ["OPENAI"]
 
 def generate_weekly_message():
-    today = datetime.date.today()
-    prompt = f"What are your Goals/ Resolutions for this week? {today}\n\nResponse:"
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # Use the GPT model you prefer
-        prompt=prompt,
-        temperature=0.7,  # Adjust temperature for creativity
-        max_tokens=150  # Adjust max_tokens as needed
-    )
-    message_content = response.choices[0].text.strip()
-    return message_content
+
+    return "THIS IS A TEST"
