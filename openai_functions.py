079e9a78e18713d0189abb3b0596d8b00a7b3aa2
jrobertson21 <josh.robertson@ibm.com>
2024-04-07 08:33:59 +0100

    testing changes

--- openai_functions.py
+++ openai_functions.py
@@ -1,22 +1,25 @@
 import os
-import openai
+from openai import OpenAI
 import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
+client = OpenAI()
+
 # Set the OpenAI key
-openai.api_key = os.environ["OPENAI"]
+OpenAI.api_key = os.environ["OPENAI"]
 
 
 def generate_weekly_message():
     today = datetime.date.today()
     prompt = f"What are your Goals/ Resolutions for this week? {today}\n\nResponse:"
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # Use the GPT model you prefer
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",  # Use the GPT model you prefer
         prompt=prompt,
-        temperature=0.7,  # Adjust temperature for creativity
-        max_tokens=150  # Adjust max_tokens as needed
+        top_p=0.3,
+        temperature=0.3,  # Adjust temperature for creativity
+        max_tokens=2  # Adjust max_tokens as needed
     )
     message_content = response.choices[0].text.strip()
     return message_content
