c9602eb0f82573edab159bcf174884cf04d13aba
Josh <63469253+joshuatrobertson@users.noreply.github.com>
2024-06-24 09:04:06 +0100

    changed contact message at bottom of post

--- tests/slack_functions_unit_tests/test_extract_emojis.py
+++ tests/slack_functions_unit_tests/test_extract_emojis.py
@@ -11,24 +11,20 @@ class TestExtractEmojis(unittest.TestCase):
         expected_emojis = []
         self.assertEqual(extract_emojis_from_message(message), expected_emojis)
 
-
     def test_extract_with_dot(self):
         message = "Test!\n1. :laughing:\n2. :sun_with_face:\n3. :moon:"
         expected_emojis = ['laughing', 'sun_with_face', 'moon']
         self.assertEqual(extract_emojis_from_message(message), expected_emojis)
-
 
     def test_extract_in_middle_of_text(self):
         message = "Test!\n1. :laughing: this is the end of a line\n2. :sun_with_face:\n3. :moon: eol"
         expected_emojis = []
         self.assertEqual(extract_emojis_from_message(message), expected_emojis)
 
-
     def test_no_emojis(self):
         message = "No emojis here!\nJust text.\nAnother line."
         expected_emojis = []
         self.assertEqual(extract_emojis_from_message(message), expected_emojis)
-
 
     def test_mixed_formats(self):
         message = "Mixed formats here!\n1: :star:\n2. :heart:\n3: :earth_americas:"
