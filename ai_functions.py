6ed159fc359fd8930d3af93f996c7bd002c32c65
Josh <63469253+joshuatrobertson@users.noreply.github.com>
2024-06-26 08:51:18 +0100

    Comment now left when pairing users

--- ai_functions.py
+++ ai_functions.py
@@ -61,7 +61,7 @@ def write_prompt(day):
         "with no additional information or notes or any preamble.")
     content = (
         f"Start by generating a Slack post for Coffee Roulette. Your response and the post should begin with 'Good "
-        f" Morning, it's Wednesday which means time for #coffee-roulette!' Mention today is {day} and ask a fun, related "
+        f" Morning, it's Wednesday before sync which means time for #coffee-roulette!' Mention today is {day} and ask a fun, related "
         f"question that asks for a preference and then provide exactly three answers on new lines that users can vote "
         f"on. Each answer must start on a new line and end with a contextually relevant emoji that matches the "
         f"sentiment or content of the answer. The answers should be concise, no more than five words each and should "
